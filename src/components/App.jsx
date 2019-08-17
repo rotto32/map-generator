f1d86935e696e34ed086258abcf37948163b17e2
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-17 09:59:02 -0700

    Add the ability to change the weights of terrain

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 
 import Board from './Board.jsx';
@@ -7,13 +8,25 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      height: 3,
-      width: 7,
+      height: 10,
+      width: 30,
       board: [],
-      terrainLib: ['/\\', '/\\', '__', '__', '〒', '〒', '〒', '~~', '*'],
+      mapTerrain: ['/\\', '/\\', '__', '__', '〒', '〒', '〒', '~~', '*'],
+      terrainLib: {
+        mountain: '/\\', plain: '__', forest: '〒', water: '~~', city: '*',
+      },
+      showTerrainSelection: false,
+      mWeight: 2,
+      pWeight: 2,
+      fWeight: 3,
+      wWeight: 1,
+      cWeight: 1,
     };
 
     this.generateBoard = this.generateBoard.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.toggleTerrain = this.toggleTerrain.bind(this);
+    this.generateTerrainFromWeights = this.generateTerrainFromWeights.bind(this);
   }
 
 
@@ -23,7 +36,7 @@ class App extends React.Component {
       const row = [];
       for (let j = 0; j < this.state.width; j++) {
         row.push(
-          this.state.terrainLib[Math.floor(Math.random() * this.state.terrainLib.length)],
+          this.state.mapTerrain[Math.floor(Math.random() * this.state.mapTerrain.length)],
         );
       }
       newBoard.push(row);
@@ -33,8 +46,37 @@ class App extends React.Component {
     });
   }
 
-  handleFormChange() {
-    console.log();
+  handleFormChange(e) {
+    const value = e.target.value || 0;
+    this.setState({
+      [e.target.name]: value,
+    });
+  }
+
+  toggleTerrain() {
+    this.setState({
+      showTerrainSelection: !this.state.showTerrainSelection,
+    });
+  }
+
+  generateTerrainFromWeights() {
+    const tLib = this.state.terrainLib;
+    const newMapTerrain = [];
+    function addToNewWeights(weight, name) {
+      for (let i = 0; i < weight; i = 1 + i) {
+        newMapTerrain.push(tLib[name]);
+      }
+    }
+
+    addToNewWeights(this.state.mWeight, 'mountain');
+    addToNewWeights(this.state.pWeight, 'plain');
+    addToNewWeights(this.state.fWeight, 'forest');
+    addToNewWeights(this.state.wWeight, 'water');
+    addToNewWeights(this.state.cWeight, 'city');
+
+    this.setState({
+      mapTerrain: newMapTerrain,
+    }, this.toggleTerrain);
   }
 
 
@@ -47,30 +89,110 @@ class App extends React.Component {
         <div>
           <form>
             Height:
-            <br />
-            <input type="number" name="height" />
+            <input
+              type="number"
+              name="height"
+              onChange={this.handleFormChange}
+            />
+            {' '}
+            {this.state.height}
             <br />
             Width:
+            <input
+              type="number"
+              name="width"
+              onChange={this.handleFormChange}
+            />
+            {' '}
+            {this.state.width}
             <br />
-            <input type="number" name="width" />
-            <br />
-            <button type="submit">Update</button>
           </form>
         </div>
+        {!this.state.showTerrainSelection ? (
+          <div>
+            <br />
+            Terrain Weights
+            <br />
+            /\ :
+            {' '}
+            {this.state.mWeight}
+            <br />
+            __ :
+            {' '}
+            {this.state.pWeight}
+            <br />
+            〒 :
+            {' '}
+            {this.state.fWeight}
+            <br />
+            ~~ :
+            {' '}
+            {this.state.wWeight}
+            <br />
+            * :
+            {' '}
+            {this.state.cWeight}
+          </div>
+        ) : (<div />)}
 
-        <div className="map-display">
-         height:
-          {' '}
-          {this.state.height}
-          {' '}
-          <br />
-         width:
-          {' '}
-          {this.state.width}
-        </div>
+        <button type="button" onClick={this.toggleTerrain}>
+          Change Terrain
+        </button>
+
+        {this.state.showTerrainSelection ? (
+          <div>
+            Please input a number from 0-5 for each of the terrain types.
+            <form>
+              Mountain (/\) weight:
+              <input
+                type="number"
+                name="mWeight"
+                onChange={this.handleFormChange}
+              />
+              <br />
+              Plain (__) weight:
+              <input
+                type="number"
+                name="pWeight"
+                onChange={this.handleFormChange}
+              />
+              <br />
+              Forest (〒) weight:
+              <input
+                type="number"
+                name="fWeight"
+                onChange={this.handleFormChange}
+              />
+              <br />
+              Water (~~) weight:
+              <input
+                type="number"
+                name="wWeight"
+                onChange={this.handleFormChange}
+              />
+              <br />
+              City (*) weight:
+              <input
+                type="number"
+                name="cWeight"
+                onChange={this.handleFormChange}
+              />
+            </form>
+            <button type="button" onClick={this.generateTerrainFromWeights}>
+              Change Terrain Weights
+            </button>
+            <button type="button" onClick={this.toggleTerrain}>
+              Close
+            </button>
+          </div>
+        ) : (
+          <div />
+        )}
 
         <button onClick={this.generateBoard}>Generate</button>
-        <div><Board board={this.state.board || ['a']} /></div>
+        <div>
+          <Board board={this.state.board || ['a']} />
+        </div>
       </div>
     );
   }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const listStyle = {
-  listStyleType: "none",
+  listStyleType: 'none',
 };
 
 function Board(props) {
   return (
     <div>
       <ul style={listStyle}>
-        {props.board.map((el, i)=>{
+        {props.board.map((el, i) => {
           let row = '';
           for (let i = 0; i < el.length; i++) {
             row += el[i];
           }
           return <li key={i}>{row}</li>;
-        })}     
+        })}
       </ul>
     </div>
   );
